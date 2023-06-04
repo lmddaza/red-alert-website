c150c43821e20d2bea090a7932a70912f849bd3d
lmddaza <lmddaza@iskolarngbayan.pup.edu.ph>
2023-06-04 14:58:44 +0800

    for database

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import NavigationBar from "src/components/top-navbar/index.js";
 import MyComponent from "src/components/slider/slider.js";
 import ImageContainers from "src/components/home/home.js";
-
 import Footer from "src/components/footer/footer.js";
+import Articles from "src/components/home/yt.js";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="page-container">
       <NavigationBar className="custom-navbar" />
       <MyComponent />
 
       <ImageContainers />
+      <Articles />
 
       <Footer />
     </div>
